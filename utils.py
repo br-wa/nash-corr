025df50c09388956a0f3e792090fadbc1700dbb6
br-wa <brwa@mit.edu>
2023-12-13 17:56:55 -0500

    add batch size for hyperparam opt

--- utils.py
+++ utils.py
@@ -2,33 +2,40 @@ import numpy as np
 import gurobipy as gp
 from gurobipy import GRB
 
-def nash_support(A, B, N, gurobienv, debug=False):
+def nash_support(A, B, N, gurobienv, batch_size=1, debug=False):
     # computes the support size of the smallest Nash equilibrium with utilty matrices A and B
-    assert A.shape == (N, N)
-    assert B.shape == (N, N)    
-
-    Ua = np.max(A) - np.min(A)
-    Ub = np.max(B) - np.min(B)
+    Ua = max([np.max(Ai) for Ai in A]) - min([np.min(Ai) for Ai in A])
+    Ub = max([np.max(Bi) for Bi in B]) - min([np.min(Bi) for Bi in B])
     ones = np.full(N, 1)
 
     with gp.Model("game", env=gurobienv) as model:
-        x = model.addMVar(N, lb=0)
-        xignored = model.addMVar(N, vtype=GRB.BINARY)
-        y = model.addMVar(N, lb=0)
-        yignored = model.addMVar(N, vtype=GRB.BINARY)
-        vA = model.addVar(lb=-float('inf'))
-        vB = model.addVar(lb=-float('inf'))
+        x = []
+        xignored = []
+        y = []
+        yignored = []
+        vA = []
+        vB = []
+        for _ in range(batch_size):
+            x.append(model.addMVar(N, lb=0))
+            xignored.append(model.addMVar(N, vtype=GRB.BINARY))
+            y.append(model.addMVar(N, lb=0))
+            yignored.append(model.addMVar(N, vtype=GRB.BINARY))
+            vA.append(model.addVar(lb=-float('inf')))
+            vB.append(model.addVar(lb=-float('inf')))
     
-        model.addConstr(A @ y <= vA)
-        model.addConstr(B.T @ x <= vB)
-        model.addConstr(A @ y >= vA - Ua * xignored)
-        model.addConstr(B.T @ x >= vB - Ub * yignored)
-        model.addConstr(ones @ x == 1)
-        model.addConstr(ones @ y == 1)
-        model.addConstr(x + xignored <= 1)
-        model.addConstr(y + yignored <= 1)
+        for i in range(batch_size):
+            model.addConstr(A[i] @ y[i] <= vA[i])
+            model.addConstr(B[i].T @ x[i] <= vB[i])
+            model.addConstr(A[i] @ y[i] >= vA[i] - Ua[i] * xignored[i])
+            model.addConstr(B[i].T @ x[i] >= vB[i] - Ub[i] * yignored[i])
+            model.addConstr(ones @ x[i] == 1)
+            model.addConstr(ones @ y[i] == 1)
+            model.addConstr(x[i] + xignored[i] <= 1)
+            model.addConstr(y[i] + yignored[i] <= 1)
     
-        model.setObjective(ones @ (xignored + yignored), GRB.MAXIMIZE)
+        
+        local_sum = [ones @ (xignored[i] + yignored[i]) for i in range(batch_size)]
+        model.setObjective(sum(local_sum), GRB.MAXIMIZE)
 
         model.optimize()
     
@@ -37,16 +44,17 @@ def nash_support(A, B, N, gurobienv, debug=False):
             print(y)
 
         if model.status == GRB.OPTIMAL:
-             sol_size = (N - model.objVal / 2)
+            sol_sizes = []
+            for i in range(batch_size):
+                
         else:
-            sol_size = 0
+            sol_sizes = [0 for _ in range(batch_size)]
 
-    return sol_size
+    return sol_sizes
 
 if __name__ == "__main__":
     with gp.Env() as env:
-        env.setParam("OutputFlag", 0)
         env.start()
         A = np.array([[0, 1, -1], [-1, 0, 1], [1, -1, 0]])
         B = -A
-        print(nash_support(A, B, 3, env, debug=True))
+        print(nash_support([A, A], [B, B], 3, env, batch_size=2, debug=True))
